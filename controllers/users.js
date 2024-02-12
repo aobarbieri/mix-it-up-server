30435b9de59a64a11b2f12193e366e0fb92b1154
aobarbieri <aobarbieri@gmail.com>
2024-02-12 17:25:26 -0500

    Define Users controllers index, create and show action

--- controllers/users.js
+++ controllers/users.js
@@ -3,5 +3,51 @@
 ////////////////////////////////
 
 const express = require('express')
-const { Users } = require('../models')
+const { User } = require('../models')
 // we can use 'object de-structuring' to access just the model we need for this controller
+
+// USER INDEX ACTION
+async function index(req, res, next) {
+	try {
+		// get all users
+		res.json(await User.find({}))
+	} catch (error) {
+		//send error
+		res.status(400).json(error)
+	}
+}
+
+// USER CREATE ACTION
+async function create(req, res, next) {
+	try {
+		// create new user
+		res.json(await User.create(req.body))
+	} catch (error) {
+		//send error
+		res.status(400).json(error)
+	}
+}
+
+// USER SHOW ACTION
+async function show(req, res, next) {
+	try {
+		// send one user
+		res.json(await User.findById(req.params.id))
+	} catch (error) {
+		//send error
+		res.status(400).json(error)
+	}
+}
+
+// ... the remaining USER controller actions will go below
+
+// USER DESTROY ACTION
+
+// USER UPDATE ACTION
+
+// EXPORT Controller Action
+module.exports = {
+	index,
+	create,
+	show,
+}
