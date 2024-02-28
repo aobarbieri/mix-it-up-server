30c7254253781c6e2134977b113e90ac09ea931b
aobarbieri <aobarbieri@gmail.com>
2024-02-28 17:09:54 -0500

    Update user Schema properties

--- models/users.js
+++ models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const validator = require('validator')
 
 // Embedding - child subdocument (favorite) is embedded within its parent document (user)
 // Here is One - to - many- relationship ---<
@@ -10,15 +11,30 @@ const favoriteSchema = new Schema(
 	{ timestamps: true }
 )
 
-const UserSchema = new Schema(
+const userSchema = new Schema(
 	{
-		name: { type: String, required: true },
-		email: { type: String, required: true, unique: true },
+		name: { type: String, required: [true, 'A name is required'] },
+		email: {
+			type: String,
+			required: [true, 'A email is required'],
+			unique: true,
+			lowercase: true,
+			validate: [validator.isEmail, 'Please provide a valid email'],
+		},
 		avatar: String,
+		password: {
+			type: String,
+			required: [true, 'Please provide a passworde'],
+			minlength: 8,
+		},
+		passwordConfirm: {
+			type: String,
+			required: [true, 'Please confirm your password'],
+		},
 		favorites: [favoriteSchema],
 		// googleId: { type: String, required: true },
 	},
 	{ timestamps: true }
 )
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', userSchema)
