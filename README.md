82436920deed0a51e0399967d29f5f5cea6df7d2
aobarbieri <aobarbieri@gmail.com>
2024-02-29 12:04:40 -0500

    Implement JSON Web Token

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Deployed backend:
 
 https://mix-it-up-server-71a37baf0155.herokuapp.com/
+
+BCRYPTJS - Password hashing
+JSON WEB TOKE (JWT) - Authentication
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,24 @@
+const jwt = require('jsonwebtoken')
 const { User } = require('../models')
 
 module.exports = {
 	signup,
+	login,
 }
 
 async function signup(req, res, next) {
-	const newUser = await User.create(req.body)
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	})
+
+	const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
 	try {
 		res.status(201).json({
 			status: 'success',
+			token,
 			data: {
 				user: newUser,
 			},
@@ -16,4 +26,8 @@ async function signup(req, res, next) {
 	} catch (error) {
 		res.status(400).json(error)
 	}
+}
+
+function login(req, res, next) {
+	const { email, password } = req.body
 }
